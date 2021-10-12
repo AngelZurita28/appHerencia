9ba1288b107b50cb73b99847a65393c745cc4165
af339 <af339@ZUWURITA>
2021-10-12 08:34:15 -0500

    Add project files.

---
+++ appHerencia/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace appHerencia
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Punto2D a;
+            a = new Punto2D(98, 76);
+            Console.WriteLine(a.ToString());
+
+            Punto a2;
+            a2 = new Punto(5.99);
+
+            Punto3D a3;
+            a3 = new Punto3D(8, 7, 9);
+            Console.WriteLine(a3.ToString());
+
+            Console.ReadKey();
+
+
+        }
+    }
+}
---
+++ appHerencia/Punto3D.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace appHerencia
+{
+    class Punto3D : Punto2D
+    {
+        private double z;
+
+        public double Z
+        {
+            get { return z; }
+            set { z = value; }
+        }
+        private double distance;
+
+        public double Distance
+        {
+            get { return distance; }
+            set { distance = value; }
+        }
+
+        public Punto3D() : base()
+        {
+            z = 0;
+        }
+        public Punto3D(double x, double y, double z) : base(x,y)
+        {
+            this.z = z;
+        }
+        public double DistanciaXY(double x, double y)
+        {
+            double d = x - y;
+            distance = d;
+            return d;
+        }
+        public override string ToString()
+        {
+            return base.ToString() + " Z=" + z + "\n distancia X-Y= " + distance;
+        }
+    }
+}
